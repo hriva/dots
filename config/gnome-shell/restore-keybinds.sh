22944b501ba50067186fda54cde68b05529103fe
hector.riv <books_thief@outlook.com>
2023-11-19 22:14:26 -0600

    fix: correct file extension in the restore-keybinds script

--- config/gnome-shell/restore-keybinds.sh
+++ config/gnome-shell/restore-keybinds.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 #dconf load /org/gnome/shell/extensions/pop-shell/ < ~/DOTFILES/config/gnome-shell/keybinds-pop-shell.conf
-dconf load /org/gnome/shell/extensions/forge/keybindings/ < ~/DOTFILES/config/gnome-shell/keybinds-forge.conf
-dconf load /org/gnome/desktop/wm/keybindings/ < ~/DOTFILES/config/gnome-shell/keybinds-wm.conf
-dconf load /org/gnome/mutter/keybindings/ < ~/DOTFILES/config/gnome-shell/keybinds-mutter.conf
-dconf load /org/gnome/mutter/wayland/keybindings/ < ~/DOTFILES/config/gnome-shell/keybinds-mutter-wayland.conf
-dconf load /org/gnome/settings-daemon/plugins/media-keys/ < ~/DOTFILES/config/gnome-shell/keybinds-settingsdaemon-mediakeys.conf
+dconf load /org/gnome/shell/extensions/forge/keybindings/ < ~/DOTFILES/config/gnome-shell/keybinds-forge.toml
+dconf load /org/gnome/desktop/wm/keybindings/ < ~/DOTFILES/config/gnome-shell/keybinds-wm.toml
+dconf load /org/gnome/mutter/keybindings/ < ~/DOTFILES/config/gnome-shell/keybinds-mutter.toml
+dconf load /org/gnome/mutter/wayland/keybindings/ < ~/DOTFILES/config/gnome-shell/keybinds-mutter-wayland.toml
+dconf load /org/gnome/settings-daemon/plugins/media-keys/ < ~/DOTFILES/config/gnome-shell/keybinds-settingsdaemon-mediakeys.toml
 
