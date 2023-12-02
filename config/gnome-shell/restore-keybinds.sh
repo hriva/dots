6784c369766619ce3eafc736a2f353adfe108d30
hector.riv <books_thief@outlook.com>
2023-12-01 19:05:08 -0600

    refactor: Swap forge in favor of pop-shell. Again.

--- config/gnome-shell/restore-keybinds.sh
+++ config/gnome-shell/restore-keybinds.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#dconf load /org/gnome/shell/extensions/pop-shell/ < ~/DOTFILES/config/gnome-shell/keybinds-pop-shell.conf
+dconf load /org/gnome/shell/extensions/pop-shell/ < ~/DOTFILES/config/gnome-shell/keybinds-pop-shell.conf
 dconf load /org/gnome/shell/extensions/forge/keybindings/ < ~/DOTFILES/config/gnome-shell/keybinds-forge.toml
 dconf load /org/gnome/desktop/wm/keybindings/ < ~/DOTFILES/config/gnome-shell/keybinds-wm.toml
 dconf load /org/gnome/mutter/keybindings/ < ~/DOTFILES/config/gnome-shell/keybinds-mutter.toml
