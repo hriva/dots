fa05f739fec17d30cd90ac84c79b082e37b103cf
hector.riv <books_thief@outlook.com>
2024-03-27 17:32:22 -0600

    feat: source pyenv function

---
+++ bashrc.d/pyenv.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+source-py-env() {
+	VENV_DIR="$HOME/Code"
+	case $1 in
+	main)
+		source "$VENV_DIR/.devel-env/bin/activate"
+		;;
+	main311)
+		source "$VENV_DIR/.devel-env-311/bin/activate"
+		;;
+	# Add more cases for additional environments
+	*)
+		echo "Error: Python environment with name $1 not found"
+		;;
+	esac
+}
