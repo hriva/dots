02afaba6352cde67ab6f0c7597dcde65c664d470
hector.riv <books_thief@outlook.com>
2024-05-24 15:09:25 -0600

    feat(btrfs): add opts and debug for creating subvolumes with no cow

--- setup/fedora/no-cow-subvolumes.sh
+++ setup/fedora/no-cow-subvolumes.sh
@@ -1,23 +1,28 @@
 #!/usr/bin/env bash
 
-if [ $UID -ne 0 ]; then
-  echo "This script most be run as root"
-fi 
-
-IMAGE_DIR=/var/lib/libvirt/images
-if [ -d $IMAGE_DIR ]; then
-  if [ -z "$(ls -A $IMAGE_DIR)" ]; then
-    echo "DIR $IMAGE_DIR must be empty"
-    exit
-  fi
-  rmdir $IMAGE_DIR
+set -o errexit
+set -o nounset
+set -o pipefail
+if [[ "${TRACE-0}" == "1" ]]; then
+	set -o xtrace
 fi
 
-btrfs subvolume create $IMAGE_DIR
-chcon -t virt_image_t $IMAGE_DIR
-chmod 722 $IMAGE_DIR
-setfacl -m u:qemu:rwx $IMAGE_DIR
-restorecon -v $IMAGE_DIR
-chattr +C $IMAGE_DIR
+if [ "$UID" -ne 0 ]; then
+	echo "This script most be run as root"
+fi
 
+IMAGE_DIR="/var/lib/libvirt/images"
+if [[ -d "$IMAGE_DIR" ]]; then
+	if [[ -z "$(ls -A "$IMAGE_DIR")" ]]; then
+		echo "DIR $IMAGE_DIR must be empty"
+		exit
+	fi
+	rm -ri "$IMAGE_DIR"
+fi
 
+btrfs subvolume create "$IMAGE_DIR"
+chcon -t virt_image_t "$IMAGE_DIR"
+chmod 722 "$IMAGE_DIR"
+setfacl -m u:qemu:rwx "$IMAGE_DIR"
+restorecon -v "$IMAGE_DIR"
+chattr +C "$IMAGE_DIR"
