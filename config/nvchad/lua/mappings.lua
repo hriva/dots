73caef34d36637389678711602a986d2c935727c
hector.riv <books_thief@outlook.com>
2024-06-10 21:26:45 -0600

    feat(nvim): primegeagen style keybinds

--- config/nvchad/lua/mappings.lua
+++ config/nvchad/lua/mappings.lua
@@ -1,10 +1,21 @@
 require("nvchad.mappings")
 local map = vim.keymap.set
 
+-- QOL
 map("n", "<C-s>", "<cmd>silent write<cr>", { desc = "File Save", noremap = true })
+map("v", "J", ":m '>+1<CR>gv-gv", { desc = "Move Selection Down" })
+map("v", "K", ":m '<-2<CR>gv-gv", { desc = "Move Selection Up" })
+map("n", "J", "mzJ`z", { desc = "Append In Place" })
+map("n", "<C-d>", "<C-d>zz", { desc = "Page Half Down Centered" })
+map("n", "<C-u>", "<C-u>zz", { desc = "Page Half Up Centered" })
+map("n", "n", "nzzzv", { desc = "Next Search and Center" })
+map("n", "N", "Nzzzv", { desc = "Prev Search and Center" })
+map("n", "Q", "<nop>")
+map("n", "x", '"_x', { noremap = true, silent = true })
+map("v", "x", '"_x', { noremap = true, silent = true })
 
 -- Pasting
-map("n", "cv", '"0p', { desc = "Paste LastYank", noremap = true })
+map("n", "cv", '"0p', { desc = "Paste Last Yank", noremap = true })
 
 -- Spelling
 map("n", "<leader>ss", ":setlocal spell spelllang=es<cr>", { desc = "Spelling es", noremap = true })
@@ -19,7 +30,7 @@ map("n", "<A-J>", "<cmd>horizontal resize -2<cr>", { desc = "Spelling Disable", 
 
 -- Gitsigns
 map("n", "<leader>do", "<cmd>Gitsigns diffthis<cr>", { desc = "Diff Open" })
-map("n", "wq", "<cmd>wincmd p | q<cr>", { desc = "Window QuitPrevious" })
+map("n", "wq", "<cmd>wincmd p | q<cr>", { desc = "Window Quit Previous" })
 
 -- Keymaps
 map("n", "<leader>wm", "<cmd>Telescope keymaps<cr>", { desc = "Wich Keymap" })
