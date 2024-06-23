81ce914e18128c38feb7cf03af82ed4d6e083d50
hector.riv <books_thief@outlook.com>
2024-06-23 16:10:36 -0600

    feat: add async formatteroverride keymapping

--- config/nvchad/lua/mappings.lua
+++ config/nvchad/lua/mappings.lua
@@ -10,9 +10,11 @@ map("n", "<C-d>", "<C-d>zz", { desc = "Page Half Down Centered" })
 map("n", "<C-u>", "<C-u>zz", { desc = "Page Half Up Centered" })
 map("n", "n", "nzzzv", { desc = "Next Search and Center" })
 map("n", "N", "Nzzzv", { desc = "Prev Search and Center" })
-map("n", "Q", "<nop>")
 map("n", "x", '"_x', { noremap = true, silent = true })
 map("v", "x", '"_x', { noremap = true, silent = true })
+map("n", "<leader>dt", ":windo diffthis<CR>", { desc = "Diff This" })
+map("n", "<C-Q>", ":q<CR>", { desc = "Quit", silent = true })
+map("n", "Q", "<nop>")
 
 -- Pasting
 map("n", "cv", '"0p', { desc = "Paste Last Yank", noremap = true })
@@ -23,10 +25,10 @@ map("n", "<leader>se", ":setlocal spell spelllang=en_us<cr>", { desc = "Spelling
 map("n", "<leader>sd", ":setlocal nospell<cr>", { desc = "Spelling Disable", noremap = true })
 
 -- Multipane sizing
-map("n", "<A-k>", "<cmd>vertical resize +2<cr>", { desc = "Spelling Disable", noremap = true })
-map("n", "<A-j>", "<cmd>vertical resize -2<cr>", { desc = "Spelling Disable", noremap = true })
-map("n", "<A-K>", "<cmd>horizontal resize +2<cr>", { desc = "Spelling Disable", noremap = true })
-map("n", "<A-J>", "<cmd>horizontal resize -2<cr>", { desc = "Spelling Disable", noremap = true })
+map("n", "<A-k>", "<cmd>vertical resize +2<cr>", { desc = "Resize Vertical +2", noremap = true })
+map("n", "<A-j>", "<cmd>vertical resize -2<cr>", { desc = "Resize Vertical -2", noremap = true })
+map("n", "<A-K>", "<cmd>horizontal resize +2<cr>", { desc = "Resize Horizontal +2", noremap = true })
+map("n", "<A-J>", "<cmd>horizontal resize -2<cr>", { desc = "Resize Horizontal -2", noremap = true })
 
 -- Gitsigns
 map("n", "<leader>do", "<cmd>Gitsigns diffthis<cr>", { desc = "Diff Open" })
@@ -35,12 +37,12 @@ map("n", "wq", "<cmd>wincmd p | q<cr>", { desc = "Window Quit Previous" })
 -- Keymaps
 map("n", "<leader>wm", "<cmd>Telescope keymaps<cr>", { desc = "Wich Keymap" })
 
--- Conform
+-- Conform Overrides
 local conform = require("conform")
 
-map("n", "<leader>cf", function()
+map("n", "<leader>fm", function()
 	conform.format({ async = true, lsp_fallback = true })
-end, { desc = "Format buffer" })
+end, { desc = "Format Files" })
 
 -- Disable mappings
 local nomap = vim.keymap.del
