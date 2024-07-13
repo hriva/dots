1c4bc6bcbfaa6f19204bcfb31484b48cbb0d7ca0
hector.riv <books_thief@outlook.com>
2024-07-13 17:57:33 -0600

    ref(pyenv): add create-stub alias

--- bashrc.d/pyenv.sh
+++ bashrc.d/pyenv.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/env bash
 
 alias pip-update="pip list | awk 'NR>2 {print $1}' | xargs pip install -U"
-alias create-stubs="pip list | awk 'NR>2 {print $1}' | xargs -I{} basedpyright --createstub {} ."
+alias list_python_modules='python -c "import pkgutil; print(\"\\n\".join([mod.name for mod in pkgutil.iter_modules()]))"'
+alias create-stubs="list_python_modules | xargs -I{} pyright --createstub {}"
 
 pyenv() {
-	VENV_DIR="$HOME/Code"
-	case $1 in
-	main)
-		source "$VENV_DIR/.devel-env/bin/activate"
-		;;
-	main311)
-		source "$VENV_DIR/.devel-env-311/bin/activate"
-		;;
-	# Add more cases for additional environments
-	*)
-		echo "Error: Python environment with name $1 not found"
-		;;
-	esac
+    VENV_DIR="$HOME/Code"
+    case $1 in
+    main)
+        source "$VENV_DIR/.devel-env/bin/activate"
+        ;;
+    main311)
+        source "$VENV_DIR/.devel-env-311/bin/activate"
+        ;;
+    # Add more cases for additional environments
+    *)
+        echo "Error: Python environment with name $1 not found"
+        ;;
+    esac
 }
