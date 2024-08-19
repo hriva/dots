ea9ff07f7ed697a1f42ae2606ada948dd896f911
hector.riv <books_thief@outlook.com>
2024-08-18 22:36:26 -0600

    chore(setup): maintain fedora pkg list

--- setup/fedora/install-packages.sh
+++ setup/fedora/install-packages.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-xargs -a $PWD/fedora-base-pkg-list.txt -d '\n' sudo dnf install -y --skip-broken
-xargs -a $PWD/fedora-rpmfusion-pkg-list.txt -d '\n' sudo dnf install -y --skip-broken
+xargs -a fedora-base-pkg-list.txt -d '\n' sudo dnf install -y --skip-broken
+xargs -a fedora-rpmfusion-pkg-list.txt -d '\n' sudo dnf install -y --skip-broken
+sudo rkhunter --update
+sudo rkhunter --propup
+sudo rkhunter --enable hidden_ports
