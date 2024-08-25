1122cf5bc23fb6568fffcf7ad95b1e2e44ba2fe0
hector.riv <books_thief@outlook.com>
2024-08-24 23:19:37 -0600

    ref(vscode): unify vscode settings

---
+++ config/vscode/show-setting.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+#
+# Shows the json inside a vscode.code-profile file
+
+set -o errexit -o nounset -o pipefail -o errtrace
+if [[ "${TRACE-0}" == "1" ]]; then
+    set -o xtrace
+fi
+
+if [[ $# -ne 2 ]]; then
+    echo "Usage: [INPUT CODE-PROF] [SETTING]"
+    exit 1
+fi
+
+case "$2" in
+*extensions)
+    sed_str='s/\\//g;s/^"//g;s/"$//g'
+    ;;
+*)
+    sed_str='s/\\\\r\\\\n/\n/g;s/\\//g;s/"{/{/g;s/}"/}/g'
+    ;;
+esac
+
+jq "$2" "$1" | sed "$sed_str" | biome format --config-path="$BIOME_CONFIG_PATH" --skip-errors --stdin-file-path file.json
