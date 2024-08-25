ec0bd5a72f905a0ec8b1bd33bad9c254e7ea284f
hector.riv <books_thief@outlook.com>
2024-08-24 20:09:10 -0600

    feat(setup): add r and r deps install for fedora

---
+++ setup/fedora/install-r.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+# Description of the script.
+sudo dnf install R --setopt=install_weak_deps=False
+sudo dnf install libcurl-devel harfbuzz-devel fribidi-devel freetype-devel libpng-devel freetype-devel libpng-devel libtiff-devel libjpeg-devel
