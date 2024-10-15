67d9e798282e22578baa08ac19560f64fa49fec1
hector.riv <books_thief@outlook.com>
2024-10-15 15:08:34 -0600

    fix(alias): escape awk arg in pip-update alias

--- bashrc.d/pyenv.sh
+++ bashrc.d/pyenv.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-alias pip-update="pip list | awk 'NR>2 {print $1}' | xargs pip install -U"
+alias pip-update="pip list | awk 'NR>2 {print \$1}' | xargs pip install -U"
 alias list_python_modules='python -c "import pkgutil; print(\"\\n\".join([mod.name for mod in pkgutil.iter_modules()]))"'
 alias create-stubs="list_python_modules | xargs -I{} pyright --createstub {}"
 
