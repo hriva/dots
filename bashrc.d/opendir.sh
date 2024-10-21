8dbd75fe20856864d16b9cc76ac1b40e5b604620
hector.riv <books_thief@outlook.com>
2024-10-21 12:27:03 -0600

    refactor(opendir): accept line for transient prompt comp

--- bashrc.d/opendir.sh
+++ bashrc.d/opendir.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
 if [[ -n "$BASH_VERSION" ]]; then
-	bind -m emacs -x '"\C-O": " __zoxide_zi\C-m"'
+    bind -m emacs -x '"\C-O": " __zoxide_zi\C-m"'
 
 elif [[ -n "$ZSH_VERSION" ]]; then
-	open_dir_vs() {
-		__zoxide_zi
-		zle reset-prompt
-	}
-	zle -N open_dir_vs
-	bindkey "^O" open_dir_vs
+    open_dir_vs() {
+        __zoxide_zi
+        zle .accept-line
+
+    }
+    zle -N open_dir_vs
+    bindkey "^O" open_dir_vs
 
 fi
