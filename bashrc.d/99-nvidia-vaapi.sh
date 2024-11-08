f4272ca5f9c135bff5dee0fded5417a3e69cd0b6
hector.riv <books_thief@outlook.com>
2024-11-08 09:50:05 -0600

    refactor(nvidia): add gbm-backend

--- bashrc.d/99-nvidia-vaapi.sh
+++ bashrc.d/99-nvidia-vaapi.sh
@@ -6,6 +6,7 @@ if grep -q "Hybrid" /etc/supergfxd.conf; then
     export NVD_BACKEND=direct
     export VDPAU_DRIVER=nvidia
     export LIBVA_DRIVER_NAME=nvidia
+    export GBM_BACKEND=nvidia-drm
     export __GLX_VENDOR_LIBRARY_NAME=nvidia
     export __EGL_VENDOR_LIBRARY_FILENAMES=/usr/share/glvnd/egl_vendor.d/10_nvidia.json
     export MOZ_DISABLE_RDD_SANDBOX=1
