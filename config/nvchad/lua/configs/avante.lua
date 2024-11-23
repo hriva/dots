3835d7b1472603fc5807c34ffde81139fff6892e
hector.riv <books_thief@outlook.com>
2024-11-23 12:50:52 -0600

    ref(nvim): new avantes for updated versions
    properly setup ollama with __inheri

--- config/nvchad/lua/configs/avante.lua
+++ config/nvchad/lua/configs/avante.lua
@@ -1,48 +1,30 @@
+-- views can only be fully collapsed with the global statusline
+vim.opt.laststatus = 3
 require("avante_lib").load()
 require("avante").setup({
 	provider = "ollama",
 	vendors = {
 		---@type AvanteProvider
 		ollama = {
-			["local"] = true,
+			-- ["local"] = true,
+			__inherited_from = "openai",
+			api_key_name = "",
 			endpoint = os.getenv("OLLAMA_REMOTE") or "127.0.0.1:11434/v1",
-			model = os.getenv("AVANTE_MODEL") or "qwen2.5-coder:1.5b",
-			parse_curl_args = function(opts, code_opts)
-				return {
-					url = opts.endpoint .. "/chat/completions",
-					headers = {
-						["Accept"] = "application/json",
-						["Content-Type"] = "application/json",
-					},
-					body = {
-						model = opts.model,
-						messages = require("avante.providers").copilot.parse_message(code_opts), -- you can make your own message, but this is very advanced
-						max_tokens = 8192,
-						stream = true,
-					},
-				}
-			end,
-			parse_response_data = function(data_stream, event_state, opts)
-				require("avante.providers").openai.parse_response(data_stream, event_state, opts)
-			end,
+			model = os.getenv("AVANTE_MODEL") or "qwen2.5-coder:latest",
+			max_tokens = 131072,
 		},
 	},
 	auto_suggestions_provider = "ollama", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
-	claude = {
-		endpoint = "https://api.anthropic.com",
-		model = "claude-3-5-sonnet-20240620",
-		temperature = 0,
-		max_tokens = 4096,
-	},
 	behaviour = {
 		auto_suggestions = false, -- Experimental stage
 		auto_set_highlight_group = true,
 		auto_set_keymaps = true,
 		auto_apply_diff_after_generation = false,
 		support_paste_from_clipboard = false,
+		minimize_diff = true,
 	},
 	mappings = {
-		--- @class AvanteConflictMappings
+		---@class AvanteConflictMappings
 		diff = {
 			ours = "co",
 			theirs = "ct",
@@ -67,8 +49,8 @@ require("avante").setup({
 			insert = "<C-s>",
 		},
 		sidebar = {
-			apply_all = "<M-A>",
-			apply_cursor = "<M-a>",
+			apply_all = "A",
+			apply_cursor = "a",
 			switch_windows = "<Tab>",
 			reverse_switch_windows = "<S-Tab>",
 		},
@@ -86,6 +68,7 @@ require("avante").setup({
 		},
 		input = {
 			prefix = "> ",
+			height = 8, -- Height of the input window in vertical layout
 		},
 		edit = {
 			border = "rounded",
@@ -93,8 +76,10 @@ require("avante").setup({
 		},
 		ask = {
 			floating = false, -- Open the 'AvanteAsk' prompt in a floating window
-			start_insert = true, -- Start insert mode when opening the ask window, only effective if floating = true.
+			start_insert = true, -- Start insert mode when opening the ask window
 			border = "rounded",
+			---@type "ours" | "theirs"
+			focus_on_apply = "ours", -- which diff to focus after applying
 		},
 	},
 	highlights = {
@@ -109,5 +94,27 @@ require("avante").setup({
 		autojump = true,
 		---@type string | fun(): any
 		list_opener = "copen",
+		--- Override the 'timeoutlen' setting while hovering over a diff (see :help timeoutlen).
+		--- Helps to avoid entering operator-pending mode with diff mappings starting with `c`.
+		--- Disable by setting to -1.
+		override_timeoutlen = 500,
+	},
+	--- @class AvanteRepoMapConfig
+	repo_map = {
+		ignore_patterns = {
+			"%.git",
+			"%.worktree",
+			"__pycache__",
+			"node_modules",
+			"%.OLD",
+			"%.bak",
+			"%.env",
+			"%.csv",
+			"%.envrc",
+			"Session.nvim",
+			"LICENSE",
+			"LICENSE.txt",
+		}, -- ignore files matching these
+		negate_patterns = {}, -- negate ignore files matching these.
 	},
 })
