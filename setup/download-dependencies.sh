f0aa26a62f8e25a9179dfa9f0bffd9fe4c4455bc
hector.riv <books_thief@outlook.com>
2025-01-25 10:30:41 -0600

    wip: precompiled binaries download

--- setup/download-dependencies.sh
+++ setup/download-dependencies.sh
@@ -4,16 +4,16 @@ set -o errexit
 set -o nounset
 set -o pipefail
 if [[ "${TRACE-0}" == "1" ]]; then
-	set -o xtrace
+    set -o xtrace
 fi
 
 if [[ ! -d ~/Packages ]]; then
-	mkdir ~/Packages
+    mkdir ~/Packages
 fi
 
 if ! which git &>/dev/null; then
-	echo "git is not installed. Please install it first."
-	exit 1
+    echo "git is not installed. Please install it first."
+    exit 1
 fi
 
 ################### MV to Packages ########################
@@ -23,8 +23,8 @@ curl https://cht.sh/:cht.sh >cht.sh
 chmod +x cht.sh
 mv cht.sh ~/.local/bin/
 
-curl -o doc2txt.zip https://sourceforge.net/projects/docx2txt/files/latest/download
-unzip doc2txt.zip
+curl -o doc2txt.tgz https://sourceforge.net/projects/docx2txt/files/latest/download
+tar -xzf doc2txt.tgz
 
 curl -o "OneDriveGUI.AppImage" https://github.com/bpozdena/OneDriveGUI/releases/latest/download/OneDriveGUI-1.0.3_fix150-x86_64.AppImage
 
@@ -35,13 +35,16 @@ mv CommitMono ~/.local/share/fonts/
 
 # shellharden binary
 curl -o shellharden.tar.gz \
-	https://github.com/anordal/shellharden/releases/latest/download/shellharden-x86_64-unknown-linux-gnu.tar.gz
+    https://github.com/anordal/shellharden/releases/latest/download/shellharden-x86_64-unknown-linux-gnu.tar.gz
 tar -xzf shellharden.tar.gz -C ~/.local/bin/
 
 # biome binary
 curl -o biome \
-	https://github.com/biomejs/biome/releases/latest/download/biome-linux-x64
+    https://github.com/biomejs/biome/releases/latest/download/biome-linux-x64
 mv biome ~/.local/bin/
+
+curl -o xsv.tar.gz https://github.com/BurntSushi/xsv/releases/latest/download/xsv-0.13.0-x86_64-unknown-linux-musl.tar.gz
+tar -xzf xsv.tar.gz -C ~/.local/bin
 
 curl https://github.com/TheZoraiz/ascii-image-converter/releases/latest/download/ascii-image-converter_Linux_amd64_64bit.tar.gz | tar -xz
 chmod u+x ascii-image-converter_Linux_amd64_64bit/ascii-image-converter
