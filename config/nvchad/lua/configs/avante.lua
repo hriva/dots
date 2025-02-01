55b43684600179c7a67d49228bbc34ab73cf1693
hector.riv <books_thief@outlook.com>
2025-02-01 16:18:35 -0600

    chore(nvim): inclue new config options. avante

--- config/nvchad/lua/configs/avante.lua
+++ config/nvchad/lua/configs/avante.lua
@@ -1,4 +1,4 @@
--- views can only be fully collapsed with the global statusline
+-- views can onlyj be fully collapsed with the global statusline
 vim.opt.laststatus = 3
 require("avante_lib").load()
 require("avante").setup({
@@ -16,51 +16,38 @@ require("avante").setup({
 	},
 	auto_suggestions_provider = "ollama", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
 	behaviour = {
+		auto_focus_sidebar = true,
 		auto_suggestions = false, -- Experimental stage
+		auto_suggestions_respect_ignore = false,
 		auto_set_highlight_group = true,
 		auto_set_keymaps = true,
 		auto_apply_diff_after_generation = false,
+		jump_result_buffer_on_finish = false,
 		support_paste_from_clipboard = false,
 		minimize_diff = true,
+		enable_token_counting = true,
 	},
-	mappings = {
-		---@class AvanteConflictMappings
+	history = {
+		max_tokens = 8192,
+		storage_path = vim.fn.stdpath("state") .. "/avante",
+		paste = {
+			extension = "png",
+			filename = "pasted-%Y%m%dT%H%M.%S",
+		},
+	},
+	highlights = {
+		---@type AvanteConflictHighlights
 		diff = {
-			ours = "co",
-			theirs = "ct",
-			all_theirs = "ca",
-			both = "cb",
-			cursor = "cc",
-			next = "]x",
-			prev = "[x",
-		},
-		suggestion = {
-			accept = "<M-l>",
-			next = "<M-]>",
-			prev = "<M-[>",
-			dismiss = "<C-]>",
-		},
-		jump = {
-			next = "]]",
-			prev = "[[",
-		},
-		submit = {
-			normal = "<CR>",
-			insert = "<C-s>",
-		},
-		sidebar = {
-			apply_all = "A",
-			apply_cursor = "a",
-			switch_windows = "<Tab>",
-			reverse_switch_windows = "<S-Tab>",
+			current = "DiffText",
+			incoming = "DiffAdd",
 		},
 	},
-	hints = { enabled = false },
 	windows = {
-		---@type "right" | "left" | "top" | "bottom"
-		position = "right", -- the position of the sidebar
+		---@alias AvantePosition "right" | "left" | "top" | "bottom" | "smart"
+		position = "right",
 		wrap = true, -- similar to vim.o.wrap
-		width = 30, -- default % based on available width
+		width = 30, -- default % based on available width in vertical layout
+		height = 30, -- default % based on available height in horizontal layout
 		sidebar_header = {
 			enabled = true, -- true, false to enable/disable the header
 			align = "center", -- left, center, right for title
@@ -76,28 +63,23 @@ require("avante").setup({
 		},
 		ask = {
 			floating = false, -- Open the 'AvanteAsk' prompt in a floating window
-			start_insert = true, -- Start insert mode when opening the ask window
 			border = "rounded",
-			---@type "ours" | "theirs"
+			start_insert = true, -- Start insert mode when opening the ask window
+			---@alias AvanteInitialDiff "ours" | "theirs"
 			focus_on_apply = "ours", -- which diff to focus after applying
 		},
 	},
-	highlights = {
-		---@type AvanteConflictHighlights
-		diff = {
-			current = "DiffText",
-			incoming = "DiffAdd",
-		},
-	},
-	--- @class AvanteConflictUserConfig
+	--- @class AvanteConflictConfig
 	diff = {
 		autojump = true,
-		---@type string | fun(): any
-		list_opener = "copen",
 		--- Override the 'timeoutlen' setting while hovering over a diff (see :help timeoutlen).
 		--- Helps to avoid entering operator-pending mode with diff mappings starting with `c`.
 		--- Disable by setting to -1.
 		override_timeoutlen = 500,
+	},
+	--- @class AvanteHintsConfig
+	hints = {
+		enabled = false,
 	},
 	--- @class AvanteRepoMapConfig
 	repo_map = {
@@ -116,5 +98,18 @@ require("avante").setup({
 			"LICENSE.txt",
 		}, -- ignore files matching these
 		negate_patterns = {}, -- negate ignore files matching these.
+	},
+	--- @class AvanteFileSelectorConfig
+	file_selector = {
+		--- @alias FileSelectorProvider "native" | "fzf" | "mini.pick" | "snacks" | "telescope" | string
+		provider = "native",
+		-- Options override for custom providers
+		provider_opts = {},
+		--- @type fun(selected_paths: string[] | string | nil): nil
+		handler = nil,
+	},
+	suggestion = {
+		debounce = 600,
+		throttle = 600,
 	},
 })
