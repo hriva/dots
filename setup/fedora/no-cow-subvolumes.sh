8b3d31df8012c40e8695e3cc5812267ec1152ee1
hector.riv <books_thief@outlook.com>
2025-04-17 13:17:44 -0600

    ref: use +C on directory, don't use subvolume

--- setup/fedora/no-cow-subvolumes.sh
+++ setup/fedora/no-cow-subvolumes.sh
@@ -4,25 +4,25 @@ set -o errexit
 set -o nounset
 set -o pipefail
 if [[ "${TRACE-0}" == "1" ]]; then
-	set -o xtrace
+    set -o xtrace
 fi
 
 if [ "$UID" -ne 0 ]; then
-	echo "This script most be run as root"
+    echo "This script most be run as root"
 fi
 
 IMAGE_DIR="/var/lib/libvirt/images"
 if [[ -d "$IMAGE_DIR" ]]; then
-	if [[ -z "$(ls -A "$IMAGE_DIR")" ]]; then
-		echo "DIR $IMAGE_DIR must be empty"
-		exit
-	fi
-	rm -ri "$IMAGE_DIR"
+    if [[ -z "$(ls -A "$IMAGE_DIR")" ]]; then
+        echo "DIR $IMAGE_DIR must be empty"
+        exit
+    fi
+    rm -ri "$IMAGE_DIR"
 fi
 
-btrfs subvolume create "$IMAGE_DIR"
+mkdir "$IMAGE_DIR"
+chattr +C "$IMAGE_DIR"
 chcon -t virt_image_t "$IMAGE_DIR"
 chmod 722 "$IMAGE_DIR"
 setfacl -m u:qemu:rwx "$IMAGE_DIR"
 restorecon -v "$IMAGE_DIR"
-chattr +C "$IMAGE_DIR"
