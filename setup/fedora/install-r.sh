73a98da497bc3aae355171479acc0e293aa92ffe
hector.riv <books_thief@outlook.com>
2025-06-21 16:59:33 -0600

    feat: fedora dependencies for R packages

--- setup/fedora/install-r.sh
+++ setup/fedora/install-r.sh
@@ -2,4 +2,4 @@
 
 # Description of the script.
 sudo dnf install R --setopt=install_weak_deps=False
-sudo dnf install libcurl-devel harfbuzz-devel fribidi-devel freetype-devel libpng-devel freetype-devel libpng-devel libtiff-devel libjpeg-devel
+sudo dnf install cmake libcurl-devel harfbuzz-devel fribidi-devel freetype-devel libpng-devel freetype-devel libpng-devel libtiff-devel libjpeg-devel unixODBC-devel libarrow-devel
