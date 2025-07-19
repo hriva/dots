7fc882f098a7b07ce09c3725a5df3ca406d11f2c
hector.riv <books_thief@outlook.com>
2025-07-18 20:01:25 -0600

    chore: fedora package list

--- setup/fedora/install-r.sh
+++ setup/fedora/install-r.sh
@@ -2,4 +2,5 @@
 
 # Description of the script.
 sudo dnf install R --setopt=install_weak_deps=False
-sudo dnf install cmake libcurl-devel harfbuzz-devel fribidi-devel freetype-devel libpng-devel freetype-devel libpng-devel libtiff-devel libjpeg-devel unixODBC-devel libarrow-devel
+
+sudo dnf install cmake libcurl-devel harfbuzz-devel fribidi-devel freetype-devel libpng-devel freetype-devel libpng-devel libtiff-devel libjpeg-devel unixODBC-devel libarrow-devel proj-devel geos-devel gdal-devel --setopt=install_weak_deps=False
