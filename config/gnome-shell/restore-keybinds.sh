6419b91a804f622a4dd5416f573c60cfbe3a8c70
alquimista <books_thief@outlook.com>
2025-12-06 18:36:59 -0600

    ref(dconf): use relative path, drop forge

--- config/gnome-shell/restore-keybinds.sh
+++ config/gnome-shell/restore-keybinds.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-dconf load /org/gnome/shell/extensions/pop-shell/ < ~/DOTFILES/config/gnome-shell/keybinds-pop-shell.toml
-dconf load /org/gnome/shell/extensions/forge/keybindings/ < ~/DOTFILES/config/gnome-shell/keybinds-forge.toml
-dconf load /org/gnome/desktop/wm/keybindings/ < ~/DOTFILES/config/gnome-shell/keybinds-wm.toml
-dconf load /org/gnome/mutter/keybindings/ < ~/DOTFILES/config/gnome-shell/keybinds-mutter.toml
-dconf load /org/gnome/mutter/wayland/keybindings/ < ~/DOTFILES/config/gnome-shell/keybinds-mutter-wayland.toml
-dconf load /org/gnome/settings-daemon/plugins/media-keys/ < ~/DOTFILES/config/gnome-shell/keybinds-settingsdaemon-mediakeys.toml
-
+dconf load /org/gnome/shell/extensions/pop-shell/ < keybinds-pop-shell.toml
+dconf load /org/gnome/desktop/wm/keybindings/ < keybinds-wm.toml
+dconf load /org/gnome/mutter/keybindings/ < keybinds-mutter.toml
+dconf load /org/gnome/mutter/wayland/keybindings/ < keybinds-mutter-wayland.toml
+dconf load /org/gnome/settings-daemon/plugins/media-keys/ < keybinds-settingsdaemon-mediakeys.toml
