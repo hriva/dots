2944283dc6b084ff7feee214eff4aa68716fb1fa
hriva <books_thief@outlook.com>
2025-12-15 10:50:50 -0600

    feat: take python version as arg on pyenv script
    change python venvs path

--- bashrc.d/pyenv.sh
+++ bashrc.d/pyenv.sh
@@ -4,25 +4,25 @@ alias pip-update="pip list | awk 'NR>2 {print \$1}' | xargs pip install -U"
 alias list_python_modules='python -c "import pkgutil; print(\"\\n\".join([mod.name for mod in pkgutil.iter_modules()]))"'
 alias create-stubs="list_python_modules | xargs -I{} pyright --createstub {}"
 
+export PYTHON_VENV_DIRS="$HOME"/.local/share/python-venvs
+
 pyenv() {
-    VENV_DIR="$HOME"/Code/.python_virt_environments
-    # venvs="$(/usr/bin/ls -a "$VENV_DIR")"
+    # venvs="$(/usr/bin/ls -a "$PYTHON_VENV_DIRS")"
     # if grep -qF "$1" <<<"$venvs"; then
-    if [ -d "$VENV_DIR"/"$1" ]; then
-        source "$VENV_DIR"/"$1"/bin/activate
+    if [ -d "$PYTHON_VENV_DIRS"/"$1" ]; then
+        source "$PYTHON_VENV_DIRS"/"$1"/bin/activate
         return 0
     fi
 
     echo "Venv $1 not found, avaliable venvs are:"
-    /usr/bin/ls -a "$VENV_DIR"
+    /usr/bin/ls -a "$PYTHON_VENV_DIRS"
     return 1
 }
 
 pyenv-make() {
-    VENV_DIR="$HOME"/Code/.python_virt_environments
-    venv_name="$VENV_DIR"/"$1"
+    venv_name="$PYTHON_VENV_DIRS"/"$1"
     if [[ ! -d "$venv_name" ]]; then
-        python3 -m venv "$venv_name"
+        python"${2:-"3"}" -m venv "$venv_name"
         source "$venv_name"/bin/activate
         pip install -U setuptools wheel pip
         deactivate
@@ -33,8 +33,7 @@ pyenv-make() {
 }
 
 pyenv-make-ls() {
-    VENV_DIR="$HOME"/Code/.python_virt_environments
-    venv_name="$VENV_DIR"/"$1"
+    venv_name="$PYTHON_VENV_DIRS"/"$1"
     if [[ ! -d "$venv_name" ]]; then
         python3.11 -m venv "$venv_name"
         source "$venv_name"/bin/activate
@@ -47,8 +46,7 @@ pyenv-make-ls() {
 }
 
 pyenv-rm() {
-    VENV_DIR="$HOME"/Code/.python_virt_environments
-    venv_name="$VENV_DIR"/"$1"
+    venv_name="$PYTHON_VENV_DIRS"/"$1"
     if [[ -d "$venv_name" ]]; then
         /usr/bin/rm -r "$venv_name"
         echo "The $1 environment has been deleted."
@@ -58,6 +56,6 @@ pyenv-rm() {
 }
 
 pyenv-ls() {
-    VENV_DIR="$HOME"/Code/.python_virt_environments
-    /usr/bin/ls -a "$VENV_DIR"
+    PYTHON_VENV_DIRS="$HOME"/Code/.python_virt_environments
+    /usr/bin/ls -a "$PYTHON_VENV_DIRS"
 }
